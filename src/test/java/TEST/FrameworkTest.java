8f249ed4befabf44885b78ffa28869babb1aa0d9
sidkhosla <sidkhosla2@gmail.com>
2021-01-15 15:26:55 +0530

    Adding test bed

--- src/test/java/TEST/FrameworkTest.java
+++ src/test/java/TEST/FrameworkTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -25,7 +24,7 @@ public class FrameworkTest {
         filmsDTO = (FilmsDTO) Injector.injectDTO("https://swapi.dev/api/", "films/1/");
         peopleDTO = (PeopleDTO) Injector.injectDTO("https://swapi.dev/api/","people/1/");
         speciesDTO = (SpeciesDTO) Injector.injectDTO("https://swapi.dev/api/","species/3/");
-        starshipsDTO = (StarshipsDTO) Injector.injectDTO("https://swapi.dev/api/","starships/9/");
+        starshipsDTO = (StarshipsDTO) Injector.injectDTO("https://swapi.dev/api/","starships/2/");
         vehiclesDTO = (VehiclesDTO) Injector.injectDTO("https://swapi.dev/api/","vehicles/4/");
     }
 
@@ -39,43 +38,36 @@ public class FrameworkTest {
     void whatHeadersDoWeGet() throws IOException, InterruptedException {
         List<String> allValues = ConnectionManager.getHeaders();
     }
-//    @Test
-//    @DisplayName("Is Luke's height 172?")
-//    void LukeHeight(){
-//        PeopleDTO peopleDTO = Injector.injectPeopleDTO("https://swapi.dev/api/people/1/");
-//        Assertions.assertEquals("172", peopleDTO.getHeight());
-//    }
-//    @Test
-//    @DisplayName("Is Luke's name correct")
-//    void LukeNameCheck(){
-//        PeopleDTO peopleDTO = Injector.injectPeopleDTO("https://swapi.dev/api/people/1/");
-//        Assertions.assertEquals("Luke Skywalker", peopleDTO.getName());
-//    }
-//    @Test
-//    @DisplayName("get the film title for Luke")
-//    void GetFilmTitle(){
-//        PeopleDTO peopleDTO = Injector.injectPeopleDTO("https://swapi.dev/api/people/1/");
-//        FilmsDTO filmsDTO = Injector.injectFilmsDTO("https://swapi.dev/api/films/1/");
-//        Assertions.assertEquals("Luke Skywalker", peopleDTO.getName());
-//    }
-//    @Test
-//    @DisplayName("CR-90-Corvette")
-//    void speedOfTheStarship(){
-//        StarshipsDTO starshipsDTO = Injector.injectStarshipsDTO("https://swapi.dev/api/starships/2/");
-//        Assertions.assertEquals("950", starshipsDTO.getMaxAtmospheringSpeed());
-////        System.out.println(starshipsDTO.getMaxAtmospheringSpeed());
-//    }
-//    @Test
-//    @DisplayName("can luke drive X-wing")
-//    void lukXWING(){
-//        String key = "12";
-//        PeopleDTO peopleDTO = Injector.injectPeopleDTO("https://swapi.dev/api/people/1/");
-//        Assertions.assertTrue(peopleDTO.getStarships().contains("http://swapi.dev/api/starships/" + key + "/"));
-//    }
+    @Test
+    @DisplayName("Is Luke's height 172?")
+    void LukeHeight(){
+        Assertions.assertEquals("172", peopleDTO.getHeight());
+    }
+    @Test
+    @DisplayName("Is Luke's name correct")
+    void LukeNameCheck(){
+        Assertions.assertEquals("Luke Skywalker", peopleDTO.getName());
+    }
+    @Test
+    @DisplayName("get the film title for Luke")
+    void GetFilmTitle(){
+        Assertions.assertEquals("Luke Skywalker", peopleDTO.getName());
+    }
+    @Test
+    @DisplayName("CR-90-Corvette")
+    void speedOfTheStarship(){
+        Assertions.assertEquals("950", starshipsDTO.getMaxAtmospheringSpeed());
+//        System.out.println(starshipsDTO.getMaxAtmospheringSpeed());
+    }
+    @Test
+    @DisplayName("can luke drive X-wing")
+    void lukXWING(){
+        String key = "12";
+        Assertions.assertTrue(peopleDTO.getStarships().contains("http://swapi.dev/api/starships/" + key + "/"));
+    }
     @Test
     @DisplayName("Person get name")
     void getName() {
-//        Assertions.assertEquals("Luke Skywalker", peopleDTO.getName());
-        System.out.println(peopleDTO.getName());
+        Assertions.assertEquals("Luke Skywalker", peopleDTO.getName());
     }
 }
